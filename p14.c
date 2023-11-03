6dcaf5cb2b59ac6086f33c4ed8bab508d6922569
kuthalingaraj6 <148896286+kuthalingaraj6@users.noreply.github.com>
2023-11-03 14:47:30 +0530

    Add files via upload

---
+++ p14.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+ int i,j,k;
+printf("Enter the row\t:");
+scanf("%d",&k);
+ for(i = 0;i<k;i++){
+   for(j = 0;j<k;j++){
+    printf("*");
+   }
+    printf("\n");
+ }
+
+
+return 0;
+}
